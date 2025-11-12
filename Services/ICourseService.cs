72680fcb692f830208ed1d18b7d509f8251b0f68
fianerd <mattiasson.sofia@gmail.com>
2025-11-12 17:09:04 +0100

    Add course interface service

---
+++ Services/ICourseService.cs
@@ -0,0 +1,13 @@
+﻿using StudentsApi.Models;
+
+namespace StudentsApi.Services
+{
+    public interface ICourseService
+    {
+        public IEnumerable<Course> GetAllCourses();
+        public Course? GetCourseById(Guid id);
+        public Course CreateCourse(Course course);
+        public Course UpdateCourse(Guid id, Course course);
+        public Course DeleteCourse(Guid id);
+    }
+}
