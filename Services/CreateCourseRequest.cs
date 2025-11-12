60fd07115f3a29c1b96239bebb65c6e863282488
fianerd <mattiasson.sofia@gmail.com>
2025-11-12 17:14:40 +0100

    Add course request for create and update

---
+++ Services/CreateCourseRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace StudentsApi.Services
+{
+    public struct CreateCourseRequest (string title, string description)
+    {
+        public string Title { get; set; } = title;
+        public string Description { get; set; } = description;
+    }
+}
--- Services/ICourseService.cs
+++ Services/ICourseService.cs
@@ -6,8 +6,8 @@ namespace StudentsApi.Services
     {
         public IEnumerable<Course> GetAllCourses();
         public Course? GetCourseById(Guid id);
-        public Course CreateCourse(Course course);
-        public Course UpdateCourse(Guid id, Course course);
+        public Course CreateCourse(CreateCourseRequest course);
+        public Course UpdateCourse(Guid id, CreateCourseRequest course);
         public Course DeleteCourse(Guid id);
     }
 }
