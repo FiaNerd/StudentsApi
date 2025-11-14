93aec4ca5e301966be91c146021a36773148e35d
fianerd <mattiasson.sofia@gmail.com>
2025-11-14 21:43:08 +0100

    Add a interface for all course instance

---
+++ Repositories/ICourseInstanceRepository.cs
@@ -0,0 +1,9 @@
+﻿using StudentsApi.Models;
+
+namespace StudentsApi.Repositories
+{
+    public interface ICourseInstanceRepository
+    {
+        public IEnumerable<CourseInstance> GetAllCourseInstance();
+    }
+}
