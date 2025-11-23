598e0126e4dc2284c4b996692d8d868f23c39f08
fianerd <mattiasson.sofia@gmail.com>
2025-11-23 16:54:54 +0100

    feat(model): Add validations for courses

--- Models/Course.cs
+++ Models/Course.cs
@@ -1,9 +1,15 @@
-﻿namespace StudentsApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsApi.Models
 {
     public class Course (string title, string description)
     {
         public Guid Id { get; init; } = Guid.NewGuid();
+
+        [Required]
         public string Title { get; set; } = title;
+
+        [Required]
         public string Description { get; set; } = description;
     }
 }
--- Models/CreateCourseRequest.cs
+++ Models/CreateCourseRequest.cs
@@ -1,8 +1,14 @@
-﻿namespace StudentsApi.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsApi.Models
 {
     public struct CreateCourseRequest (string title, string description)
     {
+        [Required]
+        [StringLength(80, MinimumLength = 2, ErrorMessage = "Title has to have at least 2 characters and can not be longer then 80 char" )]
         public string Title { get; set; } = title;
+
+        [Required]
         public string Description { get; set; } = description;
     }
 }
