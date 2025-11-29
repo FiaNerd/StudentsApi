3d453015d585ecc24bbad802b56dc8e0fca3368d
fianerd <mattiasson.sofia@gmail.com>
2025-11-29 18:04:01 +0100

    feature(dto): Add DTO for course instance

---
+++ Models/DTOs/CourseInstance.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsApi.Models.DTOs
+{
+    public class CourseInstance
+    {
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+    }
+}
