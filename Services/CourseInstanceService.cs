669b73474deaab709c44784ebd392d13b77c30b3
fianerd <mattiasson.sofia@gmail.com>
2025-11-30 19:24:37 +0100

    feat(service): Add services and add auto mapper to services and get all course instances

---
+++ Services/CourseInstanceService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using StudentsApi.Models;
+using StudentsApi.Models.DTOs;
+using StudentsApi.Repositories;
+
+namespace StudentsApi.Services
+{
+    public class CourseInstanceService : ICourseInsanceService
+    {
+        private readonly ICourseInstanceRepository _repository;
+        private readonly IMapper _mapper;
+
+        public CourseInstanceService(ICourseInstanceRepository repository, IMapper mapper )
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<CourseInstanceDTO>> GetAllCourseInstances()
+        {
+            try
+            {
+                IEnumerable<CourseInstance> courseInstances = await _repository.GetAllCourseInstances();
+
+                return _mapper.Map<IEnumerable<CourseInstanceDTO>>(courseInstances);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Services/ICourseInsanceService.cs
@@ -0,0 +1,9 @@
+﻿using StudentsApi.Models.DTOs;
+
+namespace StudentsApi.Services
+{
+    public interface ICourseInsanceService
+    {
+        public Task<IEnumerable<CourseInstanceDTO>> GetAllCourseInstances();
+    }
+}
