19ffcb5c58a6a5c8afc4471800b4b3e84ebd16a1
fianerd <mattiasson.sofia@gmail.com>
2025-12-01 22:05:57 +0100

    feat(update dto): Add validation for updateCurseDto

--- Models/DTOs/UpdateCourseDTO.cs
+++ Models/DTOs/UpdateCourseDTO.cs
@@ -1,7 +1,11 @@
-﻿namespace StudentsApi.Models.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentsApi.Models.DTOs
 {
     public class UpdateCourseDTO
     {
+        [StringLength(80, MinimumLength = 2, ErrorMessage = "Title has to have at least 2 characters and can not be longer then 80 char")]
+
         public string? Title { get; set; } = string.Empty;
 
         public string? Description { get; set; } = string.Empty;
